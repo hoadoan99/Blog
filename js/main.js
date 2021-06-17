e89b2ee2f550d92af7379353816def4cc3c2aacd
hoadoan99 <overwatch19999@gmail.com>
2021-06-17 16:54:09 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mybutton = document.getElementById("totopBtn");
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.visibility = "visible";
+    } else {
+        mybutton.style.visibility = "hidden";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
